4fe347ab46ed378293e384408f47c379bb3eda42
Leonid Stojkovski <leonid.stojkovski@gmail.com>
2020-04-12 20:07:35 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -3,16 +3,46 @@ class Student {
 	String firstName;
 	String lastName;
         int[] grades;
+	double total = 0;
 
 	//TODO constructor
+	public Student(){
+	index = 11111;
+	firstName = Petko;
+	lastName = Petkovski
+	}
 
 	//TODO seters & getters
+	public void setIndex(String index){
+	this.index = index;
+	}
+	public void setFirstName(String firstName){
+	this.firstName = firstName;
+	}
+	public void setLastName(String LastName){
+	this.LastName = LastName;
+	}
+
+	public String getIndex(){
+	return index;
+	}
+	public String getFirstName(){
+	return firstName;
+	}
+	public String getLastName(){
+	return lastName;
+	}
 
 	public double getAverage() {
 		//TODO
+	for(int i=0; i.grades.lenght; i++){
+	total = total + grades[i];
+	}
+	double average = total/arr.length;
 	}
 
 	public int ECTSCredits() {
 		//TODO
+
 	}
 }
